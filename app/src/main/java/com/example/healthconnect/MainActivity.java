c7baff5ae12a63a642d41282af9448921143a07a
aditya <adityaatri9@gmail.com>
2023-10-02 22:01:22 +0530

    Commit

--- app/src/main/java/com/example/healthconnect/MainActivity.java
+++ app/src/main/java/com/example/healthconnect/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.healthconnect;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(this, "Tere itne chuut maru ga itne chut maru salee", Toast.LENGTH_SHORT).show();
+        //This is a comment
     }
 }
